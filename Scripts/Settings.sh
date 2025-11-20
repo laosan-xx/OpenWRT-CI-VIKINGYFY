c70833d0bf5ed3b5cd2d9e49e2c1e4f3b71ac164
huYang <306061454@qq.com>
2025-11-20 11:22:52 +0800

    看看能否不开启WAN6

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -37,6 +37,15 @@ CFG_FILE="./package/base-files/files/bin/config_generate"
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
 #修改默认主机名
 sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
+#禁用wan6接口
+if [ -f "$CFG_FILE" ]; then
+	#注释掉创建wan6接口的相关代码行（排除已注释的行）
+	sed -i '/^[^#]*ucidef_set_interface_wan6/s/^/#/g' $CFG_FILE
+	sed -i '/^[^#]*set_interface.*wan6/s/^/#/g' $CFG_FILE
+	sed -i '/^[^#]*wan6.*proto/s/^/#/g' $CFG_FILE
+	#注释掉其他可能创建wan6的代码行
+	sed -i '/^[^#]*wan6/s/^/#/g' $CFG_FILE
+fi
 
 #配置文件修改
 echo "CONFIG_PACKAGE_luci=y" >> ./.config
