32df77e12b73b55018fd265c279627793fada060
laosan-xx <xxhy1989@gmail.com>
2025-12-20 13:42:59 +0800

    雅典娜

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -61,7 +61,7 @@ echo "已创建wan6禁用脚本: $WAN6_DISABLE_SCRIPT"
 echo "CONFIG_PACKAGE_luci=y" >> ./.config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
-echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
+# echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
 #手动调整的插件
 if [ -n "$WRT_PACKAGE" ]; then
