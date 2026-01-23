57dd9c5c550e038052b2cdd2abe5c9f3cca1dc52
laosan-xx <xxhy1989@gmail.com>
2026-01-23 23:58:39 +0800

    更新部分依赖

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -51,6 +51,7 @@ UPDATE_PACKAGE "aurora-config" "eamonxg/luci-app-aurora-config" "master"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "master"
 UPDATE_PACKAGE "kucat-config" "sirpdboy/luci-app-kucat-config" "master"
 
+UPDATE_PACKAGE "v2ray-geodata" "laosan-xx/my-v2ray-geodata" "master"
 UPDATE_PACKAGE "frp" "laosan-xx/my-frp" "main"
 # UPDATE_PACKAGE "frpc" "laosan-xx/luci-app-frpc" "main"
 UPDATE_PACKAGE "homeproxy" "laosan-xx/homeproxy" "master"
@@ -119,5 +120,7 @@ UPDATE_VERSION() {
 }
 
 #UPDATE_VERSION "软件包名" "测试版，true，可选，默认为否"
-# UPDATE_VERSION "sing-box"
+UPDATE_VERSION "geoview"
+UPDATE_VERSION "sing-box"
+UPDATE_VERSION "xray"
 #UPDATE_VERSION "tailscale"
