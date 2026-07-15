7ac082d7b9b7baf6bcc6681688bf4c98adfd7888
laosan-xx <xxhy1989@gmail.com>
2026-07-15 17:22:47 +0800

    添加 cloudflarespeedtest

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -83,6 +83,7 @@ UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "timecontrol" "sirpdboy/luci-app-timecontrol" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "axonhub gecoosac luci-app-timewol luci-app-wolplus"
+UPDATE_PACKAGE "cloudflarespeedtest" "kimmax603/luci-app-cloudflarespeedtest" "main"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
 #更新软件包版本
